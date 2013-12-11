e17313c102d2eccf1f4fd22cdf47614865705003
Brad Gunn <brad@honestbuildings.com>
2013-12-11 15:11:43 -0500

    V2 refactor for the fun of it

--- php/rock-paper-scissors/run.php
+++ php/rock-paper-scissors/run.php
@@ -1,11 +1,6 @@
 <?php
-/** Welcome to my code test, please see the attached documentation "README.md"
- *
- * Enjoy.
- *
- */
+/* Rock Paper Scissors */
 
-$validChoices = [ 'rock', 'paper', 'scissors' ];
 $scoreBoard = [ 'player' => 0, 'computer' => 0 ];
  
 echo "
@@ -35,30 +30,11 @@ while( ( $line = stream_get_line( STDIN, null, PHP_EOL ) ) !== false )
         echo "Computer:" . $scoreBoard['computer'] . PHP_EOL . PHP_EOL;
         continue;
     }
-    
-    $computerChoice = getChoice();
-    
-    if( $line == $computerChoice )
-    {
-        echo "Tie!";
-    } else {
-        switch($line)
-        {
-            case 'rock':
-                $outcome = $computerChoice == 'paper';
-                break;
-            case 'paper':
-                $outcome = $computerChoice == 'scissors';
-                break;
-            case 'scissors':
-                $outcome = $computerChoice == 'rock';
-                break;
-            default:
-                echo 'Invalid Choice!' . PHP_EOL;
-                continue;
-        }
-
-        if( isset( $outcome ) )
+    try{
+        $computersChoice = getChoice();
+        echo "Computer Got $computersChoice" . PHP_EOL;
+        $outcome = compare( $computersChoice, $line );
+        if( !is_null( $outcome ) )
         {
             if( $outcome )
             {
@@ -68,10 +44,18 @@ while( ( $line = stream_get_line( STDIN, null, PHP_EOL ) ) !== false )
                 echo "Player Wins!" . PHP_EOL;
                 $scoreBoard['player']++;
             }
+        } else {
+            echo "Tie!" . PHP_EOL;
         }
+    } catch( Exception $e ) {
+        echo $e->getMessage() . PHP_EOL;
     }
 }
 
+/** A simple function to return a randomized choice
+ *
+ * @return string the choice of rock, paper, or scissors
+ */
 function getChoice()
 {
     $random = mt_rand(1, 100);
@@ -80,4 +64,30 @@ function getChoice()
     if( $random < 66 )
         return 'paper';
     return 'scissors';
+}
+
+/** A simple comparsion operator that returns true if choice2 beats choice1
+ * False if choice1 beats choice2, and null if they are equivalent similar to
+ * string comparison functions
+ *
+ * @param string $choice1 The first choice to compare to the second choice
+ * @param string $choice2 The second choice to compare to the first choice
+ * @return boolean If choice2 beats choice1;
+ *
+ */
+function compare($choice1, $choice2)
+{
+    if( $choice1 == $choice2 )
+        return null;
+    else {
+        switch($choice2){
+            case 'rock':
+                return $choice1 == 'paper';
+            case 'paper':
+                return $choice1 == 'scissors';
+            case 'scissors':
+                return $choice1 == 'rock';
+        }
+    }
+    throw new Exception("Invalid Choice", 400);
 }
