99f85f987fbf7a4853796decd72b1cee59c375b2
Brad Gunn <brad@honestbuildings.com>
2014-01-10 22:32:10 -0500

    Birchbox algorithm solved in ~5minutes

---
+++ php/birchbox/README.md
@@ -0,0 +1,4 @@
+Let’s say you are given an array of n integers, which represents the prices of a stock over n consecutive days. Please write a function in the language of your choice that returns the biggest loss you could incur over those n days by buying high and selling low.
+
+
+For example: if the array contains |14|1|18|23|12|8|16|, then the biggest possible loss is $15 (buying at $23 and selling at $8).
---
+++ php/birchbox/run.php
@@ -0,0 +1,19 @@
+<?php
+
+$array = [ 14,1,18,23,12,8,16,24,22 ]; // Potential Max Loss in this array should be 15.
+
+$max = $lowest = array_shift( $array );
+$maxLoss = 0;
+foreach( $array as $key => $value ){
+    if( $value >= $max ) {
+        $max = $lowest = $value;
+    } else {
+        $lowest = min( $lowest, $value );
+    }
+    $potentialLoss = $max - $lowest;
+    if( $potentialLoss > $maxLoss )
+        $maxLoss = $max - $lowest;
+}
+
+echo $maxLoss;
+
